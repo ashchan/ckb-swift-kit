d23199e44a9f32d07aca810a86ef2afb1f9123da
James Chen <james@ashchan.com>
2018-12-13 14:21:35 +0900

    Create Xcode project

---
+++ Source/CKB.h
@@ -0,0 +1,19 @@
+//
+//  CKB.h
+//  CKB
+//
+//  Created by James Chen on 2018/12/13.
+//  Copyright © 2018 Nervos Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CKB.
+FOUNDATION_EXPORT double CKBVersionNumber;
+
+//! Project version string for CKB.
+FOUNDATION_EXPORT const unsigned char CKBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKB/PublicHeader.h>
+
+
