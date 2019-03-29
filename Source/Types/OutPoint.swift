b5365818bca692b1ed0a1f49ff7f41c7257388ad
cezres <cezr@sina.com>
2019-03-29 15:24:41 +0800

    chore: modify `OutPoint.init` protection level

--- Source/Types/OutPoint.swift
+++ Source/Types/OutPoint.swift
@@ -12,6 +12,11 @@ public struct OutPoint: Codable, Param {
     let hash: H256
     let index: UInt32
 
+    public init(hash: String, index: UInt32) {
+        self.hash = hash
+        self.index = index
+    }
+
     public var param: [String: Any] {
         return [
             "hash": hash,
