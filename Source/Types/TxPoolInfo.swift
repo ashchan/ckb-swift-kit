6432afc84d9917ac8fc831a2a69d675badbb6f87
James Chen <james@ashchan.com>
2019-05-08 19:55:36 +0900

    feat: Implement tx_pool_info RPC method

---
+++ Source/Types/TxPoolInfo.swift
@@ -0,0 +1,23 @@
+//
+//  TxPoolInfo.swift
+//  CKB
+//
+//  Created by James Chen on 2019/05/08.
+//  Copyright © 2019 Nervos Foundation. All rights reserved.
+//
+
+import Foundation
+
+public struct TxPoolInfo: Codable {
+    public let pending: UInt32
+    public let staging: UInt32
+    public let orphan: UInt32
+    public let lastTxsUpdatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case pending
+        case staging
+        case orphan
+        case lastTxsUpdatedAt = "last_txs_updated_at"
+    }
+}
