28bf93a30e56e66b711884f139b8aef2a700a0dc
James Chen <james@ashchan.com>
2019-05-08 11:48:46 +0900

    feat: Generalize OutPoint struct to reference headers

--- Source/Types/OutPoint.swift
+++ Source/Types/OutPoint.swift
@@ -9,6 +9,32 @@
 import Foundation
 
 public struct OutPoint: Codable, Param {
+    public let blockHash: H256?
+    public let cell: CellOutPoint?
+
+    enum CodingKeys: String, CodingKey {
+        case blockHash = "block_hash"
+        case cell
+    }
+
+    public init(blockHash: H256, cell: CellOutPoint) {
+        self.blockHash = blockHash
+        self.cell = cell
+    }
+
+    public var param: [String: Any] {
+        var result = [String: Any]()
+        if let blockHash = blockHash {
+            result["block_hash"] = blockHash
+        }
+        if let cell = cell {
+            result["cell"] = cell
+        }
+        return result
+    }
+}
+
+public struct CellOutPoint: Codable, Param {
     public let txHash: H256
     public let index: UInt32
 
--- Tests/API/APIMockingTests.swift
+++ Tests/API/APIMockingTests.swift
@@ -50,7 +50,10 @@ class APIMockingTests: XCTestCase {
     }
 
     func testGetLiveCell() throws {
-        let outPoint = OutPoint(txHash: "0xda27a795e14067e18a0cfc0951571aaca47bd9851adc9bf5baa22cf27c8bcde8", index: 0)
+        let outPoint = OutPoint(
+            blockHash: "0x4c2f8ba5f5a0104eaf84fcbb16af4b0e7ca2f2fdb076e748d54ef876d085d49e",
+            cell: CellOutPoint(txHash: "0xda27a795e14067e18a0cfc0951571aaca47bd9851adc9bf5baa22cf27c8bcde8", index: 0)
+        )
         let result = try getClient(json: "liveCellEmpty").getLiveCell(outPoint: outPoint)
         XCTAssertNotNil(result)
     }
