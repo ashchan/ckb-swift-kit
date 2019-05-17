a23b165a7d7d18b7896c0cf714a231cb063c97ab
James Chen <james@ashchan.com>
2019-05-17 18:33:05 +0900

    chore: Update iOS example

--- Examples/Example-iOS/Example-iOS/ViewController.swift
+++ Examples/Example-iOS/Example-iOS/ViewController.swift
@@ -10,35 +10,82 @@ import UIKit
 import CKB
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nodeUrlTextField: UITextField!
+    @IBOutlet weak var infoTextView: UITextView!
+    var nodeUrl: URL {
+        return URL(string: nodeUrlTextField.text!) ?? URL(string: "http://localhost:8114")!
+    }
+    var timer: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        do {
-            try testNodeInfo()
-            try testSendCapacity()
-        } catch let err {
-            print(err.localizedDescription)
+        infoTextView.layer.borderWidth = 1.0 / UIScreen.main.scale
+        infoTextView.layer.borderColor = UIColor.lightGray.cgColor
+    }
+
+    @IBAction func checkNodeInfo(_ sender: Any) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 10.0, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
+        timer!.fire()
+    }
+
+    @objc private func tick() {
+        let node = nodeUrl
+        DispatchQueue.global().async {
+            self.getNodeInfo(node)
+        }
+    }
+
+    private func showInfo(_ info: String) {
+        DispatchQueue.main.async {
+            self.infoTextView.text = info
+            self.infoTextView.layer.borderColor = UIColor.green.cgColor
+        }
+    }
+
+    private func showError(_ error: String) {
+        DispatchQueue.main.async {
+            self.infoTextView.text = error
+            self.infoTextView.layer.borderColor = UIColor.red.cgColor
         }
     }
 }
 
-extension ViewController {
-    func testNodeInfo() throws {
-        let nodeUrl = URL(string: "http://localhost:8114")!
+private extension ViewController {
+    func getNodeInfo(_ nodeUrl: URL) {
         let apiClient = APIClient(url: nodeUrl)
 
-        // Fetch local node info
-        let nodeInfo = try apiClient.localNodeInfo()
-        print(nodeInfo.version)                        // "0.12.0-pre (rylai30-1-g3e765560 2019-05-16)"
+        do {
+            let nodeInfo = try apiClient.localNodeInfo()
+            let tipHeader = try apiClient.getTipHeader()
 
-        // Get current height
-        let height = try apiClient.getTipBlockNumber() // Numbers are represented as strings
-        print(height)
+            let result = """
+            Node info:
+            \tURL: \(nodeUrl.absoluteString)
+            \tVersion: \(nodeInfo.version)
+
+            Current tip header:
+            \tEpoch: \(tipHeader.epoch)
+            \tBlock number: \(tipHeader.number)
+            \tTime: \(Date(timeIntervalSince1970: TimeInterval(tipHeader.timestamp)! / 1000).description(with: .current))
+            """
+
+            showInfo(result)
+        } catch let err {
+            let error = """
+            Fail to get node info!
+            Error: \(err.localizedDescription)
+
+            Make sure you input a correct node URL.
+            """
+
+            showError(error)
+        }
     }
 
     func testSendCapacity() throws {
         // Fetch system script which we'll use to generate lock for address
-        let nodeUrl = URL(string: "http://localhost:8114")!
         let systemScript = try SystemScript.loadFromGenesisBlock(nodeUrl: nodeUrl)
         // Fill in the sender's private key
         let privateKey: Data = Data(hex: "your private key (hex string)")
