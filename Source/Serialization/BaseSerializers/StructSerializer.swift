65305661ac0cd21be0a1885d1e508904fb69a4eb
James Chen <james@ashchan.com>
2019-09-05 21:37:22 +0900

    feat: Implement StructSerializer

--- Source/Serialization/BaseSerializers/StructSerializer.swift
+++ Source/Serialization/BaseSerializers/StructSerializer.swift
@@ -5,3 +5,21 @@
 //
 
 import Foundation
+
+/// The struct is a fixed-size type: all fields in struct are fixed-size and it has a fixed quantity of fields.
+/// The size of a struct is the sum of all fields' size.
+struct StructSerializer: Serializer {
+    private let fieldSerializers: [Serializer]
+
+    var header: [Byte] {
+        return []
+    }
+
+    var body: [Byte] {
+        return fieldSerializers.map { $0.serialize() }.reduce([], +)
+    }
+
+    init(_ fieldSerializers: [Serializer]) {
+        self.fieldSerializers = fieldSerializers
+    }
+}
--- Tests/Serialization/BaseSerializers/StructSerializerTests.swift
+++ Tests/Serialization/BaseSerializers/StructSerializerTests.swift
@@ -5,27 +5,27 @@
 //
 
 import XCTest
+@testable import CKB
 
 class StructSerializerTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testOnlyAByte() {
+        struct OnlyAByte { var f1: Byte }
+        let object = OnlyAByte(f1: 0xab)
+        let onlyAByteSerializer = StructSerializer([ByteSerializer(value: object.f1)])
+        XCTAssertEqual([Byte(0xab)], onlyAByteSerializer.serialize())
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testByteAndUint32() {
+        struct ByteAndUint32 {
+            var f1: Byte
+            var f2: UInt32
         }
+        let object = ByteAndUint32(f1: 0xab, f2: 66051/*0x010203*/)
+        let byteAndUint32Serializer = StructSerializer([
+            ByteSerializer(value: object.f1),
+            UInt32Serializer(value: object.f2)
+        ])
+        let expected: [Byte] = [0xab, 0x03, 0x02, 0x01, 0x00]
+        XCTAssertEqual(expected, byteAndUint32Serializer.serialize())
     }
-
 }
