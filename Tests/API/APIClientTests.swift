5ac302622ffd560909de7cf802424e8fdc946971
James Chen <james@ashchan.com>
2020-03-07 17:08:22 +0900

    Comment out indexer module tests

--- Tests/API/APIClientTests.swift
+++ Tests/API/APIClientTests.swift
@@ -204,6 +204,7 @@ class APIClientTests: RPCTestSkippable {
         }
     }
 
+    /*
     func testIndexLockHash() throws {
         let lockHash = "0x9a9a6bdbc38d4905eace1822f85237e3a1e238bb3f277aa7b7c8903441123510"
         var result = try client.indexLockHash(lockHash: lockHash, indexFrom: 0)
@@ -238,7 +239,7 @@ class APIClientTests: RPCTestSkippable {
         let result = try client.getTransactionsByLockHash(lockHash: "0x9a9a6bdbc38d4905eace1822f85237e3a1e238bb3f277aa7b7c8903441123510", page: 0, pageSize: 2, reverseOrder: false)
         XCTAssertNotNil(result)
         XCTAssert(result.count >= 0)
-    }
+    }*/
 }
 
 private extension APIClientTests {
